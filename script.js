0aa5af879a325638f25709363cc71e973dfafdb8
Elias Galvão Alves <132005740+EliaxZen@users.noreply.github.com>
2023-11-11 12:45:21 -0300

    adicionando funções

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
-//Dados
-let altura = 1.80;
-let peso = 70;
-
 //Cálculo 
-let imc = peso / (Math.pow(altura, 2));
-console.log(imc.toFixed(2));
-
-//Verificação
-if (imc < 18.5) {
-  console.log("Abaixo do peso")
-} else if (imc >= 18.5 && imc <= 25) {
-  console.log("Peso normal")
-} else if (imc >= 25 && imc <= 30) {
-  console.log("Acima do peso")
-} else if (imc >= 30 && imc <= 40) {
-  console.log("Obeso")
-} else {
-  console.log("Obesidade grave")
+function calcularImc(peso, altura) {
+  return peso / (Math.pow(altura, 2));
 }
+//Verificação
+function classificarImc(imc) {
+  if (imc < 18.5) {
+    return "Abaixo do peso"
+  } else if (imc >= 18.5 && imc <= 25) {
+    return "Peso normal"
+  } else if (imc >= 25 && imc <= 30) {
+    return "Acima do peso"
+  } else if (imc >= 30 && imc <= 40) {
+    return "Obeso"
+  } else {
+    return "Obesidade grave"
+  }
+}
+
+// Main
+(function () {
+  //Dados
+  let altura = 1.80;
+  let peso = 70;
+
+  let imc = calcularImc(peso, altura)
+  console.log(classificarImc(imc));
+})();
 
