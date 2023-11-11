499a3e9544d1ff49fe59d5232af6f5db3793473b
Elias Galvão Alves <132005740+EliaxZen@users.noreply.github.com>
2023-11-11 15:19:27 -0300

    adding POO
    Adicionei um poquinho de POO, adicionei métodos e uma verificação mais detalhada

--- script.js
+++ script.js
@@ -1,29 +1,37 @@
-//Cálculo 
-function calcularImc(peso, altura) {
-  return peso / (Math.pow(altura, 2));
-}
-//Verificação
-function classificarImc(imc) {
-  if (imc < 18.5) {
-    return "Abaixo do peso"
-  } else if (imc >= 18.5 && imc <= 25) {
-    return "Peso normal"
-  } else if (imc >= 25 && imc <= 30) {
-    return "Acima do peso"
-  } else if (imc >= 30 && imc <= 40) {
-    return "Obeso"
-  } else {
-    return "Obesidade grave"
+class Pessoa {
+  nome;
+  peso;
+  altura;
+
+  constructor(nome, peso, altura) {
+    this.nome = nome;
+    this.peso = peso;
+    this.altura = altura;
+  }
+
+  calcularImc() {
+    return this.peso / (Math.pow(this.altura, 2));
+  }
+
+  verificarImc() {
+    const imc = this.calcularImc();
+
+    if (imc < 18.5) {
+      return `Me chamo ${this.nome}, tenho ${this.peso}Kg de peso corporal e ${this.altura} de altura, possuo um IMC de ${imc.toFixed(2)} e estou abaixo do peso`
+    } else if (imc >= 18.5 && imc <= 25) {
+      return `Me chamo ${this.nome}, tenho ${this.peso}Kg de peso corporal e ${this.altura} de altura, possuo um IMC de ${imc.toFixed(2)} e estou com o peso ideal`
+    } else if (imc >= 25 && imc <= 30) {
+      return `Me chamo ${this.nome}, tenho ${this.peso}Kg de peso corporal e ${this.altura} de altura, possuo um IMC de ${imc.toFixed(2)} e estou acima do peso`
+    } else if (imc >= 30 && imc <= 40) {
+      return `Me chamo ${this.nome}, tenho ${this.peso}Kg de peso corporal e ${this.altura} de altura, possuo um IMC de ${imc.toFixed(2)} e estou obeso`
+    } else {
+      return `Me chamo ${this.nome}, tenho ${this.peso}Kg de peso corporal e ${this.altura} de altura, possuo um IMC de ${imc.toFixed(2)} e estou com obesidade grave`
+    }
   }
 }
 
-// Main
-(function () {
-  //Dados
-  let altura = 1.80;
-  let peso = 70;
+const pessoa1 = new Pessoa ('Elias', 80, 1.75);
+console.log(pessoa1.verificarImc());
 
-  let imc = calcularImc(peso, altura)
-  console.log(classificarImc(imc));
-})();
+
 
